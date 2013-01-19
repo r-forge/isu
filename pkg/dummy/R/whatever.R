6093e4cdea9524da1324aa3320f30f5d8800f69e
yihui (Yihui Xie) <xie@yihui.name>
2013-01-19 02:02:22 +0000

    install only if not installed already
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/isu@26 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dummy/R/whatever.R
+++ pkg/dummy/R/whatever.R
@@ -1,2 +1,4 @@
-print('nothing here!')
-install.packages(c('markdown', 'R2SWF'), repos = 'http://cran.r-project.org')
+print(.libPaths())
+if (!require('markdown')) install.packages('markdown', repos = 'http://cran.r-project.org')
+if (!require('R2SWF')) install.packages('R2SWF', repos = 'http://cran.r-project.org')
+print(.packages(TRUE))
