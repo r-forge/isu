cd245a509d5bf56dc77d14de9e40591a8423e632
yihui (Yihui Xie) <xie@yihui.name>
2013-01-19 17:44:20 +0000

    this hack should work for *nix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/isu@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dummy/R/whatever.R
+++ pkg/dummy/R/whatever.R
@@ -1,4 +1,3 @@
 print(.libPaths())
 if (!require('markdown')) install.packages('markdown', repos = 'http://cran.r-project.org')
 if (!require('R2SWF')) install.packages('R2SWF', repos = 'http://cran.r-project.org')
-print(.packages(TRUE))
